f8653740019b1e160c7e64868edbefacf092395e
andresmauricio <andresacelas@outlook.com>
2022-01-03 15:25:46 -0500

    set layout config

--- apps/01-shopping-cart/src/app/app.tsx
+++ apps/01-shopping-cart/src/app/app.tsx
@@ -3,6 +3,7 @@ import { products as initialProduct } from '../mocks/products';
 import { Cart } from './components/Cart';
 import { Navbar } from './components/Navbar';
 import ProductList from './components/ProductList';
+import './app.css';
 
 export function App() {
   const [products, setProducts] = useState(initialProduct);
@@ -10,12 +11,14 @@ export function App() {
   return (
     <>
       <Navbar count={cart.length} />
-      <ProductList
-        products={products as any}
-        cart={cart as any}
-        setCart={setCart as any}
-      ></ProductList>
-      <Cart products={cart} setCart={setCart as any} />
+      <section className="container">
+        <ProductList
+          products={products as any}
+          cart={cart as any}
+          setCart={setCart as any}
+        ></ProductList>
+        <Cart products={cart} setCart={setCart as any} />
+      </section>
     </>
   );
 }
