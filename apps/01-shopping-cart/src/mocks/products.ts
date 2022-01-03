a66aadb9c8f6a52dd1f5af98ae1374b4414b653a
andresmauricio <andresacelas@outlook.com>
2022-01-03 14:09:19 -0500

    add mock for list of products

---
+++ apps/01-shopping-cart/src/mocks/products.ts
@@ -0,0 +1,74 @@
+export const products = [
+  {
+    name: 'Truffle - Peelings',
+    id: 'fa38769c-5785-4d5b-9945-f3699dff9737',
+    description: 'Galah',
+    image:
+      'https://robohash.org/nullaexercitationemsit.png?size=50x50&set=set1',
+    price: 80.37,
+  },
+  {
+    name: 'Crab - Meat',
+    id: '13f5fdc4-c0c8-4f79-aed4-5afb3ff37483',
+    description: 'Small-spotted genet',
+    image: 'https://robohash.org/itaqueattenetur.png?size=50x50&set=set1',
+    price: 23.83,
+  },
+  {
+    name: 'Milkettes - 2%',
+    id: '62b36e70-b8e2-44e4-9cfa-0174c37c0157',
+    description: 'Bushpig',
+    image: 'https://robohash.org/accusantiumporroea.png?size=50x50&set=set1',
+    price: 33.34,
+  },
+  {
+    name: 'Potatoes - Peeled',
+    id: '9aebfeb0-3ca0-44ab-b4d2-0140bd238fda',
+    description: 'Lizard, goanna',
+    image: 'https://robohash.org/minimarerumnatus.png?size=50x50&set=set1',
+    price: 74.9,
+  },
+  {
+    name: 'Beer - Steamwhistle',
+    id: 'e750c1d9-0f3f-4c78-b64c-f8d183e3f5b3',
+    description: 'Coqui partridge',
+    image: 'https://robohash.org/eaqueetdoloremque.png?size=50x50&set=set1',
+    price: 88.94,
+  },
+  {
+    name: 'Dried Cranberries',
+    id: '3031e98d-9f67-455c-81ed-6fa09b3d0e96',
+    description: 'Black-backed jackal',
+    image: 'https://robohash.org/nequedelenitiut.png?size=50x50&set=set1',
+    price: 66.55,
+  },
+  {
+    name: 'Rum - Dark, Bacardi, Black',
+    id: '2781f8a6-b8a5-4f20-80c6-2bb9a51efee2',
+    description: 'Galapagos mockingbird',
+    image:
+      'https://robohash.org/molestiaererumexpedita.png?size=50x50&set=set1',
+    price: 23.64,
+  },
+  {
+    name: 'Bread - Corn Muffaleta Onion',
+    id: 'ee81efce-d072-424b-a95b-f5d0bdb695d1',
+    description: 'Springhare',
+    image: 'https://robohash.org/eosblanditiisullam.png?size=50x50&set=set1',
+    price: 60.86,
+  },
+  {
+    name: 'Coffee - Espresso',
+    id: 'e0334176-8f83-4c8e-9046-f2b01ae6ed77',
+    description: 'Silver-backed jackal',
+    image: 'https://robohash.org/quiadebitisasperiores.png?size=50x50&set=set1',
+    price: 52.69,
+  },
+  {
+    name: 'Tuna - Canned, Flaked, Light',
+    id: '13c2611c-54f8-45b6-97ce-c2b12cc6c448',
+    description: 'Seal, southern elephant',
+    image: 'https://robohash.org/quaerataniminam.png?size=50x50&set=set1',
+    price: 14.8,
+  },
+];
