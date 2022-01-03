5f7ec66ec361e17ea0253a019d4f4cfa96201e2c
andresmauricio <andresacelas@outlook.com>
2022-01-03 14:11:01 -0500

    delete boilerplate

--- apps/01-shopping-cart/src/app/app.tsx
+++ apps/01-shopping-cart/src/app/app.tsx
@@ -1,12 +1,9 @@
 import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="01-shopping-cart" />
-      <div />
-    </>
+    <h1>Test</h1>
+
   );
 }
 
