c48fa82f19ffeb23fe36c7e3bcfa7e8370c6c5e8
andresmauricio <andresacelas@outlook.com>
2022-01-03 16:36:23 -0500

    create project admin patients

---
+++ apps/02-admin-patients-app/src/app/app.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="02-admin-patients-app" />
+    </StyledApp>
+  );
+}
+
+export default App;
