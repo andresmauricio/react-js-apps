8be2c7c2482cac7bca30e453d791266874fd6992
andresmauricio <andresacelas@outlook.com>
2022-01-03 13:53:17 -0500

    initial commit

---
+++ apps/01-shopping-cart/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: '01-shopping-cart',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/01-shopping-cart',
+};
