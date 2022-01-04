22225ed211b935c91b2d8919f37d20001fdce757
andresmauricio <andresacelas@outlook.com>
2022-01-03 21:42:09 -0500

    global styles

--- apps/02-admin-patients-app/src/app/app.tsx
+++ apps/02-admin-patients-app/src/app/app.tsx
@@ -1,19 +1,22 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 import Form from './components/Form';
+import GlobalStyle from './components/GlobalsStyles';
 import ListAppoinments from './components/ListAppoinments';
 
-const StyledApp = styled.div`
-  // Your style here
+const Main = styled.main`
+  width: 100%;
+  height: 100vh;
 `;
 
 export function App() {
   const [appointment, setAppointment] = useState([]);
   return (
-    <>
+    <Main>
+      <GlobalStyle />
       <Form setAppointment={setAppointment} appointment={appointment} />
       <ListAppoinments appointment={appointment} />
-    </>
+    </Main>
   );
 }
 
---
+++ apps/02-admin-patients-app/src/app/components/GlobalsStyles.tsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+ 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: #2c2b3f;
+  }
+`;
+ 
+export default GlobalStyle;
