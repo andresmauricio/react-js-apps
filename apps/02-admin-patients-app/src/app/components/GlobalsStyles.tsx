8a81591033309007f8e4b864a2a7ea7ccda08490
andresmauricio <andresacelas@outlook.com>
2022-01-04 07:10:09 -0500

    add global style

--- apps/02-admin-patients-app/src/app/components/GlobalsStyles.tsx
+++ apps/02-admin-patients-app/src/app/components/GlobalsStyles.tsx
@@ -1,11 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
- 
+
 const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     padding: 0;
     background-color: #2c2b3f;
+    color: #fff;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
   }
 `;
- 
+
 export default GlobalStyle;
