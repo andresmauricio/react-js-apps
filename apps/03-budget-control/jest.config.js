21443d9bbf4c169cc0620eda67647a5ab5f25c5d
andresmauricio <andresacelas@outlook.com>
2022-01-04 17:09:57 -0500

    create project budget control

---
+++ apps/03-budget-control/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: '03-budget-control',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/03-budget-control',
+};
