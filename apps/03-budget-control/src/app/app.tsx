26ee17d4219be8298c50ba739469f849fd7082e0
andresmauricio <andresacelas@outlook.com>
2022-01-04 21:32:05 -0500

    delete boilerplate

--- apps/03-budget-control/src/app/app.tsx
+++ apps/03-budget-control/src/app/app.tsx
@@ -1,15 +1,8 @@
 import styled from 'styled-components';
-import NxWelcome from './nx-welcome';
-
-const StyledApp = styled.div`
-  // Your style here
-`;
 
 export function App() {
   return (
-    <StyledApp>
-      <NxWelcome title="03-budget-control" />
-    </StyledApp>
+   <h1>Hello</h1>
   );
 }
 
