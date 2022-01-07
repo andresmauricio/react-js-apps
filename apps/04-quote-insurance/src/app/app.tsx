0c7d55768ab98f40fd010ed5ef36970e7e5fd0b5
andresmauricio <andresacelas@outlook.com>
2022-01-07 07:46:12 -0500

    create project quote insurance

---
+++ apps/04-quote-insurance/src/app/app.tsx
@@ -0,0 +1,13 @@
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="04-quote-insurance" />
+      <div />
+    </>
+  );
+}
+
+export default App;
