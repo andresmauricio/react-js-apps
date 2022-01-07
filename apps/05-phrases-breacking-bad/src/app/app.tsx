8bb7160aecf36afa9ebc914ea85a74b635cbfc96
andresmauricio <andresacelas@outlook.com>
2022-01-07 18:59:55 -0500

    globals styles application

--- apps/05-phrases-breacking-bad/src/app/app.tsx
+++ apps/05-phrases-breacking-bad/src/app/app.tsx
@@ -1,15 +1,29 @@
+import { useState } from 'react';
 import styled from 'styled-components';
-import NxWelcome from './nx-welcome';
+import GlobalStyle from './components/GlobalStyles';
 
-const StyledApp = styled.div`
-  // Your style here
+const Main = styled.main`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #365a00;
+`;
+
+const Title = styled.h2`
+  font-weight: bold;
+  font-size: 26px;
+  color: white;
 `;
 
 export function App() {
+  const [phrase, setPhrase] = useState('');
   return (
-    <StyledApp>
-      <NxWelcome title="05-phrases-breacking-bad" />
-    </StyledApp>
+    <Main>
+      <GlobalStyle />
+      <Title>Phrases project by breacking bad</Title>
+    </Main>
   );
 }
 
