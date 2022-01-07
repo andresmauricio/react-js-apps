f066fc84b08e852fbb8811de2c37001d21c2c5a7
andresmauricio <andresacelas@outlook.com>
2022-01-07 18:51:33 -0500

    create project phrasesby breacking bad

---
+++ apps/05-phrases-breacking-bad/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: '05-phrases-breacking-bad',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/05-phrases-breacking-bad',
+};
