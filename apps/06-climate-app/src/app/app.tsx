5c214e4f3feb60653745c4c19a761884676a323d
andresmauricio <andresacelas@outlook.com>
2022-01-08 11:12:35 -0500

    get information from api

--- apps/06-climate-app/src/app/app.tsx
+++ apps/06-climate-app/src/app/app.tsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 const StyledApp = styled.div`
   // Your style here
 `;
 
+const Input = styled.input`
+  // Your style here
+`;
+
 export function App() {
+  const [city, setCity] = useState('');
+  const [currentWeather, setCurrentWeather] = useState({});
+
+  const getCurrentWeather = async () => {
+    const API_KEY = 'a1c919548b510275b471fbcdcdd7192a';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const data = await fetch(url);
+    const response = await data.json();
+    setCurrentWeather(response);
+  };
   return (
     <StyledApp>
-      <h1>New element go</h1>
-      <p>Hello</p>
+      <Input
+        placeholder="Write your city"
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <button onClick={() => getCurrentWeather()}>Search</button>
     </StyledApp>
   );
 }
