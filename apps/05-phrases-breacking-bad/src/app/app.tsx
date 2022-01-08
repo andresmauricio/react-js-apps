44729183aaa06bb05bbd8bbc0a99726e5698fed4
andresmauricio <andresacelas@outlook.com>
2022-01-07 21:21:37 -0500

    fixed styles

--- apps/05-phrases-breacking-bad/src/app/app.tsx
+++ apps/05-phrases-breacking-bad/src/app/app.tsx
@@ -6,6 +6,8 @@ const Main = styled.main`
   width: 100%;
   height: 100vh;
   display: flex;
+  flex-direction: column;
+  gap: 20px;
   align-items: center;
   justify-content: center;
   background-color: #365a00;
@@ -13,6 +15,12 @@ const Main = styled.main`
 
 const Title = styled.h2`
   font-weight: bold;
+  font-size: 36px;
+  color: white;
+`;
+
+const Phrase = styled.p`
+  font-weight: 500;
   font-size: 26px;
   color: white;
 `;
@@ -22,7 +30,8 @@ export function App() {
   return (
     <Main>
       <GlobalStyle />
-      <Title>Phrases project by breacking bad</Title>
+      <Title>Breacking Bad</Title>
+      <Phrase>Example Phrase</Phrase>
     </Main>
   );
 }
