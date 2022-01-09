7898fbd2d04d91ff0c1c255ee309cf6676b36572
andresmauricio <andresacelas@outlook.com>
2022-01-09 15:48:00 -0500

    get result

--- apps/07-crypto/src/app/app.tsx
+++ apps/07-crypto/src/app/app.tsx
@@ -10,17 +10,34 @@ const COUNTRY_CODES = ['COP', 'USD', 'CAD', 'EUR'];
 const CRYPTO_CURRENCY = ['BTC', 'ETH'];
 
 export function App() {
-
   const [currency, setCurrency] = useState('');
   const [crypto, setCryto] = useState('');
   const [response, setResponse] = useState({});
 
+  const searchCrypto = async () => {
+    if (currency && crypto) {
+      const url = `https://min-api.cryptocompare.com/data/price?fsym=${crypto}&tsyms=${currency}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setResponse(data[currency]);
+      return;
+    }
+  };
+
   return (
     <StyledApp>
       <GlobalStyle />
-      <Select options={COUNTRY_CODES} label="Select your currency" onChange={setCurrency}/>
-      <Select options={CRYPTO_CURRENCY} label="Select your crypto" onChange={setCryto}/>
-      <button>Search</button>      
+      <Select
+        options={COUNTRY_CODES}
+        label="Select your currency"
+        onChange={setCurrency}
+      />
+      <Select
+        options={CRYPTO_CURRENCY}
+        label="Select your crypto"
+        onChange={setCryto}
+      />
+      <button onClick={() => searchCrypto()}>Search</button>
     </StyledApp>
   );
 }
